9ef87f51fd87a8d8280f331842486d7c943f4f8c
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2021-11-19 22:42:56 +0100

    Ajout de commentaires et debug du script de performances

--- src/image.py
+++ src/image.py
@@ -30,7 +30,7 @@ class Image:
     def display(self, window_name):
         """Affichage a l'ecran d'une image"""
         fig = plt.figure(window_name)
-        if (not (self.pixels is None)):
+        if not (self.pixels is None):
             io.imshow(self.pixels)
             io.show()
         else:
@@ -44,7 +44,6 @@ class Image:
     #   on retourne une nouvelle image binarisee
     # ==============================================================================
     def binarisation(self, S):
-
         # creation d'une image vide
         im_bin = Image()
 
@@ -53,8 +52,10 @@ class Image:
         # sont mises a 0
         im_bin.set_pixels(np.zeros((self.H, self.W), dtype=np.uint8))
 
+        # Parcours du tableau
         for i in range(self.H):
             for j in range(self.W):
+                # si la valeur du pixel actuel est en dessous du seuil, affecte 0, sinon 255
                 im_bin.pixels[i][j] = 0 if self.pixels[i][j] < S else 255
 
         return im_bin
@@ -68,43 +69,19 @@ class Image:
     #   on retourne une nouvelle image recadree
     # ==============================================================================
     def localisation(self):
-
-        """
-        c_min = []
-        c_max = []
-        l_min = []
-        l_max = []
-        for i in range(self.H):
-            for j in range(self.W):
-                if self.pixels[i][j] == 0:
-                    c_min.append(j)
-                    break
-        
-        for i in range(self.H - 1, 0, -1):
-            for j in range(self.W):
-                if self.pixels[i][j] == 0:
-                    c_max.append(j)
-                    break
-
-        for i in range(self.W):
-            for j in range(self.H):
-                if self.pixels[j][i] == 0:
-                    l_min.append(j)
-                    break
-
-        for i in range(self.W - 1, 0, -1):
-            for j in range(self.H):
-                if self.pixels[j][i] == 0:
-                    l_max.append(j)
-                    break"""
-
+        # Instanciation des variables de découpe
         c_min = self.W - 1
         c_max = 0
         l_min = self.H - 1
         l_max = 0
+
         for i in range(self.H):
             for j in range(self.W):
+                # Si le pixel actuel est noir
                 if self.pixels[i][j] == 0:
+                    # Si la valeur de j est inférieure à c_min cela signifie
+                    # que l'on rencontre un pixel noir à un index inférieur
+                    # que la dernière valeur de c_min stockée précédemment.
                     if j < c_min:
                         c_min = j
                     if j > c_max:
@@ -114,7 +91,9 @@ class Image:
                     if i > l_max:
                         l_max = i
 
+        # On instacie un nouvel objet Image
         img = Image()
+        # On rempli l'objet avec le nouveau tableau découpé
         img.set_pixels(self.pixels[l_min:l_max + 1, c_min:c_max + 1])
         return img
 
@@ -130,12 +109,17 @@ class Image:
     # Methode de mesure de similitude entre l'image self et un modele im
     # ==============================================================================
     def similitude(self, im):
+        # Si les deux images ne sont pas de même taille
+        if (im.W == 0 and im.H == 0) or (self.W == 0 and self.H == 0):
+            return 0
         if not (im.W == self.W and im.H == self.H):
+            # On redimensionne l'autre image
             im = im.resize(self.H, self.W)
         cpt = 0
 
         for i in range(self.H):
             for j in range(self.W):
+                # Si les pixels de même position possèdent la même valeur
                 if self.pixels[i][j] == im.pixels[i][j]:
                     cpt += 1
 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-'''
+"""
 File: main.py
 Created Date: Friday August 27th 2021 - 02:35pm
 Author: Ammar Mian
@@ -8,42 +8,41 @@ Last Modified: Mon Aug 30 2021
 Modified By: Ammar Mian
 -----
 Copyright (c) 2021 Université Savoie Mont-Blanc
-'''
+"""
 import matplotlib.pyplot as plt
 from image import Image
 from reconnaissance import reconnaissance_chiffre, lecture_modeles
 
 if __name__ == '__main__':
-
     # Variables utiles
     path_to_assets = '../assets/'
-    plt.ion()  # Mode interactif de matplotlib our ne pas bloquer l'éxécutions lorsque l'on fait display
-
+    # plt.ion()  # Mode interactif de matplotlib our ne pas bloquer l'éxécutions lorsque l'on fait display
+    # Nous avons désactivé le mode interactif sinon cela ne fonctionne pas sur linux avec pycharm
     # ==============================================================================
     # Lecture image et affichage
     # ==============================================================================
     image = Image()
     image.load(path_to_assets + 'test1.JPG')
-    image.display("Exemple d'image")
+    # image.display("Exemple d'image")
 
     # ==============================================================================
     # Binarisation de l'image et affichage
     # ==============================================================================
-    S = 70
+    S = 230
     image_binarisee = image.binarisation(S)
-    image_binarisee.display("Image binarisee")
+    # image_binarisee.display("Image binarisee")
 
     # ==============================================================================
     # Localisation de l'image et affichage
     # ==============================================================================
     image_localisee = image_binarisee.localisation()
-    image_localisee.display("Image localisee")
+    # image_localisee.display(f"Image localisee de taille {image_localisee.H}x{image_localisee.W}")
 
     # ==============================================================================
     # Redimensionnement de l'image et affichage
     # ==============================================================================
     image_resizee = image_localisee.resize(100, 500)
-    image_resizee.display("Image redimensionee")
+    # image_resizee.display("Image redimensionee")
 
     # ==============================================================================
     # Lecture modeles et reconnaissance
@@ -52,9 +51,11 @@ if __name__ == '__main__':
     chiffre = reconnaissance_chiffre(image, liste_modeles, 255)
     print("Le chiffre reconnu est : ", chiffre)
 
-    fichiers = [('test1.jpg', 4), ('test2.jpg', 1), ('test3.jpg', 2), ('test4.jpg', 2), ('test5.jpg', 2),
-                ('test6.jpg', 4), ('test7.jpg', 5),
+    fichiers = [('test1.JPG', 4), ('test2.JPG', 1), ('test3.JPG', 2), ('test4.JPG', 2), ('test5.JPG', 2),
+                ('test6.JPG', 4), ('test7.JPG', 5),
                 ('test10.jpg', 6)]
+
+    # Chargement des images
     liste_imgs = []
     for fichier, nbr in fichiers:
         model = Image()
@@ -62,15 +63,18 @@ if __name__ == '__main__':
         liste_imgs.append((model, nbr))
 
     res = []
-    for s in range(0, 256, 10):
+    seuils = [*range(0, 256, 10)]
+    # Evolution du seuil
+    for s in seuils:
         l = []
-        for img, nbr in liste_imgs:
-            chiffre = reconnaissance_chiffre(image, liste_modeles, 255)
-            l.append(chiffre == nbr)
+        for img, c in liste_imgs:
+            chiffre = reconnaissance_chiffre(img, liste_modeles, s)
+            l.append(chiffre == c)
         res.append(l)
-
+    # Cumul des bonnes réponse
     res_sim = []
     for s in res:
         res_sim.append(sum(s))
 
-    print(res_sim)
+    print(seuils)  # Liste des seuils
+    print(res_sim)  # Liste des résultats
