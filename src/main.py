0e6d202c8ec102c5a14a5940cd7ae690c4528cc6
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2021-11-19 14:00:38 +0100

    Adding gitgnore + Reformating files

--- src/main.py
+++ src/main.py
@@ -13,42 +13,40 @@ import matplotlib.pyplot as plt
 from image import Image
 from reconnaissance import reconnaissance_chiffre, lecture_modeles
 
-
 if __name__ == '__main__':
-
     # Variables utiles
     path_to_assets = '../assets/'
-    plt.ion() # Mode interactif de matplotlib our ne pas bloquer l'éxécutions lorsque l'on fait display
+    plt.ion()  # Mode interactif de matplotlib our ne pas bloquer l'éxécutions lorsque l'on fait display
 
-    #==============================================================================
+    # ==============================================================================
     # Lecture image et affichage
-    #==============================================================================
+    # ==============================================================================
     image = Image()
     image.load(path_to_assets + 'test2.JPG')
     image.display("Exemple d'image")
 
-    #==============================================================================
+    # ==============================================================================
     # Binarisation de l'image et affichage
-    #==============================================================================
+    # ==============================================================================
     S = 70
     image_binarisee = image.binarisation(S)
     image_binarisee.display("Image binarisee")
 
-    #==============================================================================
+    # ==============================================================================
     # Localisation de l'image et affichage
-    #==============================================================================
+    # ==============================================================================
     image_localisee = image_binarisee.localisation()
     image_localisee.display("Image localisee")
 
-    #==============================================================================
+    # ==============================================================================
     # Redimensionnement de l'image et affichage
-    #==============================================================================
+    # ==============================================================================
     image_resizee = image_localisee.resize(100, 500)
     image_resizee.display("Image redimensionee")
 
-    #==============================================================================
+    # ==============================================================================
     # Lecture modeles et reconnaissance
-    #==============================================================================
+    # ==============================================================================
     liste_modeles = lecture_modeles(path_to_assets)
     chiffre = reconnaissance_chiffre(image, liste_modeles, 70)
     print("Le chiffre reconnu est : ", chiffre)
--- src/reconnaissance.py
+++ src/reconnaissance.py
@@ -1,8 +1,9 @@
 from image import Image
 
+
 def lecture_modeles(chemin_dossier):
-    fichiers= ['_0.png','_1.png','_2.png','_3.png','_4.png','_5.png','_6.png', 
-            '_7.png','_8.png','_9.png']
+    fichiers = ['_0.png', '_1.png', '_2.png', '_3.png', '_4.png', '_5.png', '_6.png',
+                '_7.png', '_8.png', '_9.png']
     liste_modeles = []
     for fichier in fichiers:
         model = Image()
@@ -13,4 +14,3 @@ def lecture_modeles(chemin_dossier):
 
 def reconnaissance_chiffre(image, liste_modeles, S):
     pass
-
